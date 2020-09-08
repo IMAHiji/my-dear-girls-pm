cbb46786e7d47a51a97132996ea85bb3472cf289
Luke Askins <lukeaskins@gmail.com>
2020-09-08 06:09:54 -0500

    Index page query now returns descending

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -18,54 +18,24 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
-const IndexPage = ({ data: { allPrismicPost, prismicHomepage } }: any) => {
+const Posts = ({ data: { allPrismicPost, prismicHomepage } }: any) => {
   const posts = allPrismicPost.edges;
-  const homePageData = prismicHomepage.data;
-  const homepageHero = homePageData.homepage_hero.fluid.src;
-  const { homeHeroWrapper, homeText } = useStyles(homepageHero);
 
   return (
     <>
       <Layout>
         <Grid container justify="center" alignItems="center">
-          <Grid
-            item
-            xs={12}
-            className={homeHeroWrapper}
-            justify="center"
-            container
-            style={{
-              backgroundImage: `url(${homepageHero})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'bottom 5% center',
-              alignItems: 'center',
-            }}
-          >
-            <Typography variant="h1" color="textPrimary">
-              {homePageData.title.text}
-            </Typography>
-          </Grid>
-
-          <Grid item xs={10} justify="center" alignContent="center" alignItems="center" container direction="row">
-            <Grid item xs={12}>
-              {homePageData.content.text && (
-                <Typography variant="body1" className={homeText}>
-                  {homePageData.content.text}
-                </Typography>
-              )}
-            </Grid>
-            <PostPreview nodes={posts} />
-          </Grid>
+          <p>I am list of posts</p>
         </Grid>
       </Layout>
     </>
   );
 };
 
-export default IndexPage;
+export default Posts;
 
 export const indexPageQuery = graphql`
-  query postList {
+  query posts {
     prismicHomepage {
       data {
         content {
@@ -85,7 +55,7 @@ export const indexPageQuery = graphql`
         }
       }
     }
-    allPrismicPost(sort: { order: DESC, fields: data___date }) {
+    allPrismicPost {
       edges {
         node {
           uid
